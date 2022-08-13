e133c85655c04fd75bba2756fc2392f7ed465842
cobaltgit <criterion@chitco.co.uk>
2022-08-13 20:32:30 +0100

    Use fs/promises

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,33 +1,35 @@
-const fs = require('node:fs')
-const path = require('node:path')
+const fs = require('fs/promises');
+const path = require('node:path');
 
-const { REST } = require('@discordjs/rest')
-const { Routes } = require('discord-api-types/v9')
-const { clientId, guildId, token } = require('./config.json')
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
 
-const commands = []
-const commandsPath = path.join(__dirname, 'commands')
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith('.js'))
+const commandsPath = path.join(__dirname, 'commands');
+const commands = [];
+fs.readdir(commandsPath)
+  .then((files) => {
+    files
+      .filter((file) => path.extname(file) === '.js')
+      .forEach((file) => {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        commands.push(command);
+      });
+  })
+  .catch(console.error);
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file)
-  const command = require(filePath)
-  commands.push(command.data.toJSON())
-}
+const rest = new REST({ version: '10' }).setToken(token);
 
-const rest = new REST({ version: '10' }).setToken(token)
-
-;(async () => {
-  console.log('Refreshing application (/) commands...')
+(async () => {
+  console.log('Refreshing application (/) commands...');
 
   try {
     await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
       body: commands
-    })
-    console.log('Successfully refreshed application (/) commands.')
+    });
+    console.log('Successfully refreshed application (/) commands.');
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-})()
+})();
